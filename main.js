455079c34dc447d151924b158e956d0633c7f0c9
sil-co <79182349+sil-co@users.noreply.github.com>
2022-04-17 01:39:08 +0900

    add : add notification method

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
 
+// 使用するDOM
+const todoItems = getDom(".todo-items", true);
+const todoItems2 =getDom(".todo-items");
+const editId =getDom("#edit");
+const editTitleValue =getDom(".edit-title-value");
+const editMainValue =getDom(".edit-main-value");
+const editDate =getDom(".edit-date");
+const btn =getDom("#add");
+const todoRemove =getDom(".todo-remove");
+const todoSave =getDom(".todo-save");
+const closeButton =getDom("#close");
+const todoComp =getDom(".todo-comp");
+const todoWrapper = getDom(".todo-container");
+const headerName = getDom(".header-name");
+const todoName = getDom(".todo-name");
+const addName = getDom(".add-container");
+const editContainer = getDom(".edit-container");
+const notificationContainer = getDom(".notification-container");
+const notificationValue = getDom(".notification-value");
+console.log(notificationValue.innerHTML);
+const todoItemName = document.getElementsByClassName("todo-item-name");
+const todoItem = document.getElementsByClassName("todo-item");
+const todoDate = document.getElementsByClassName("todo-date");
+const todoItemsId = document.getElementById("todo-items");
+
+// global変数
+let jsonTodoData;  // To Do Listのデータ
+let dataId; // data ID
+let headerNameNowColor;  // header name color
+
+// load後 data取得し描画
+window.addEventListener("load", getData);
+
 // dom取得関数
 function getDom(attrName, all = false) {
   let dom;
@@ -10,27 +43,49 @@ function getDom(attrName, all = false) {
   return dom;
 }
 
-// 使用するDOM
-const todoItems = document.getElementsByClassName("todo-items");
-const todoItems2 =getDom(".todo-items");
-const todoItemName = document.getElementsByClassName("todo-item-name");
-const todoItem = document.getElementsByClassName("todo-item");
-const editContainer =getDom("#edit");
-const todoDate = document.getElementsByClassName("todo-date");
-const editTitleValue =getDom(".edit-title-value");
-const editMainValue =getDom(".edit-main-value");
-const editDate =getDom(".edit-date");
-const btn =getDom("#add");
-const todoRemove =getDom(".todo-remove");
-const todoSave =getDom(".todo-save");
-const todoItemsId = document.getElementById("todo-items");
-const closeButton =getDom("#close");
-const todoComp =getDom(".todo-comp");
+// change color method
+function domChangeWordColor(dom, setColor) {
+  dom.animate(
+    {
+      color: setColor,
+    },
+    {
+      duration: 300,
+      fill: "forwards"
+    }
+  );
+}
 
+// change header name color
+function changeColor() {
+  let setColor;
 
-// global変数
-let jsonTodoData;  // To Do Listのデータ
-let dataId;
+  switch(headerNameNowColor) {
+    case "red":
+      setColor = ['red', 'blue'];
+      headerNameNowColor = 'blue';
+      break;
+    case "blue":
+      setColor = ['blue', 'green'];
+      headerNameNowColor = 'green';
+      break;
+    case "green":
+      setColor = ['green', 'pink'];
+      headerNameNowColor = 'pink';
+      break;
+    default:
+      setColor = ['#007bff', 'red'];
+      headerNameNowColor = 'red';
+  };
+  domChangeWordColor(headerName, setColor);
+  // for(const todo of todoItemName) {
+  //   domChangeWordColor(todo, setColor);
+  // }
+  // domChangeWordColor(todoName, setColor);
+  // domChangeWordColor(addName, setColor);
+}
+headerName.addEventListener("click", changeColor);
+
 
 // html文字列をnode形式に変換する関数
 function createElementFromHTML(html) {
@@ -56,7 +111,6 @@ function getData() {
   }
   jsonTodoData = JSON.parse(jsonTodoData);
   const jsonLen = Object.keys(jsonTodoData).length;
-  console.log(jsonLen);
   for(let i = 0; i < jsonLen; i++) {
     const jsonTitle = jsonTodoData[i].title;
     const jsonMain = jsonTodoData[i].main;
@@ -66,17 +120,49 @@ function getData() {
       : createElementFromHTML(`<div class="todo-item unfinish-item"><input type="datetime-local" name="schedule" min="2000-01-01T00:00" max="2100-12-31T23:59" class="todo-date" value="${jsonDate}" /><span class="todo-item-name">${jsonTitle}</span><span class="completed">完</span></div>`);
     todoItems[0].appendChild(newTodo);
     todoItemName[i].addEventListener("click", function() {
-      editContainer.classList.add("active");
+      editId.classList.add("active");
       editTitleValue.value = jsonTitle;
       editMainValue.value = jsonMain;
       editDate.value = jsonDate;
       dataId = i;
+      todoWrapper.animate(
+        {
+          opacity: [0.5, 0.8],
+        },
+        {
+          duration: 300,
+        }
+      );
     });
   }
 }
 
-// load後 data取得し描画
-window.addEventListener("load", getData);
+// notification
+function notify(val = '更新しました。') {
+  notificationValue.innerHTML = val;
+  notificationContainer.classList.add("active");
+  setTimeout(() => {
+    notificationContainer.animate(
+      {
+        opacity: [1, 0]
+      },
+      {
+        duration: 700,
+        fill: "forwards"
+      }
+      );
+      setTimeout(() => {notificationContainer.classList.remove("active");}, 1000);
+    }, 2500);
+  notificationContainer.animate(
+    {
+      opacity: [0, 1]
+    },
+    {
+      duration: 500,
+      fill: "forwards"
+    }
+  );
+}
 
 // add card method
 btn.addEventListener("click", function() {
@@ -98,11 +184,20 @@ btn.addEventListener("click", function() {
     const jsonTitle = jsonTodoData[jsonLen].title;
     const jsonMain = jsonTodoData[jsonLen].main;
     const jsonDate = jsonTodoData[jsonLen].date;
-    editContainer.classList.add("active");
+    editId.classList.add("active");
     editTitleValue.value = jsonTitle;
     editMainValue.value = jsonMain;
     editDate.value = jsonDate;
+    todoWrapper.animate(
+      {
+        opacity: [0.5, 0.8],
+      },
+      {
+        duration: 300,
+      }
+    );
   })
+  notify('カードを追加しました。');
 });
 
 // remove method
@@ -112,7 +207,8 @@ todoRemove.addEventListener("click", function() {
   localStorage.setItem("Todo", saveJsonData);
   todoItemsId.innerHTML = '';
   getData();
-  editContainer.classList.remove("active");
+  editId.classList.remove("active");
+  notify('削除しました。');
 });
 
 // save method
@@ -129,7 +225,8 @@ todoSave.addEventListener("click", function() {
   localStorage.setItem("Todo",saveJsonData);
   todoItemsId.innerHTML = '';
   getData();
-  editContainer.classList.remove("active");
+  editId.classList.remove("active");
+  notify('保存しました。');
 });
 
 // complete method
@@ -140,11 +237,12 @@ todoComp.addEventListener("click", function(){
   localStorage.setItem("Todo", JSON.stringify(jsonTodoData));
   todoItemsId.innerHTML = '';
   getData();
-  editContainer.classList.remove("active");
+  editId.classList.remove("active");
+  notify('完了しました。');
 });
 
 // close method
 closeButton.addEventListener("click", function() {
-  editContainer.classList.remove("active");
+  editId.classList.remove("active");
 });
 
